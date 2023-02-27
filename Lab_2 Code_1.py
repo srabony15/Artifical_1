ce8fae9a21dd741cd0b6e230faff86c9211be21f
Srabony <90834705+srabony15@users.noreply.github.com>
2023-02-27 11:06:21 +0600

    Add files via upload

---
+++ Lab_2 Code_1.py
@@ -0,0 +1,18 @@
+class Student:
+    ID = input("Enter the ID No: ")
+    COURSES = input("Enter the course name:")
+
+Srabony = Student()
+print(Srabony.ID)
+print(Srabony.COURSES)
+
+
+def __init__(self, ID, COURSES):
+    self.name = 'name'
+    self.age = id
+    
+    print("Hello my name is " + self.name)
+
+p1 = 'student'("srabony","200103020015" )
+p1.myfunc()
+ 
